3888351aec04a898716d18959aeb3961d56017d8
m-jimengo <elpowerrangerverdemolamas@gmail.com>
2025-10-08 01:47:10 +0200

    new micro

---
+++ user-directory-service/src/main/java/es/tsumeapps/user_directory_service/UserDirectoryServiceApplication.java
@@ -0,0 +1,13 @@
+package es.tsumeapps.user_directory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDirectoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDirectoryServiceApplication.class, args);
+	}
+
+}
---
+++ user-directory-service/src/test/java/es/tsumeapps/user_directory_service/UserDirectoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.tsumeapps.user_directory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDirectoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
